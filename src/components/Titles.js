34bb60db0dc387028062a6545a90212c70cf3b17
Ivy <ivyhan@alumni.ubc.ca>
2021-01-03 12:21:59 -0800

    init

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Titles extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <h1 className="title-container__title">Daily Forecast</h1>
+                <h3 className="title-container__subtitle">Find out temperature conditions</h3>
+            </div>
+        );
+    }
+}
+
+export default Titles;
+
